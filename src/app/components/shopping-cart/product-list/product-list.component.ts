f968fdc22357f951b21475864811685d88466d28
Jorge <jorgediazok@gmail.com>
2020-06-08 03:42:12 -0300

    finish today. Added bucle to productlist

--- src/app/components/shopping-cart/product-list/product-list.component.ts
+++ src/app/components/shopping-cart/product-list/product-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
+  productList: Product[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.productList = this.productService.getProducts();
   }
-
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  products = [{}];
+  products: Product[] = [
+    new Product(
+      1,
+      'Camiseta 1',
+      'This is the first camiseta. New, lovely materials',
+      100
+    ),
+    new Product(
+      2,
+      'Camiseta 2',
+      'This is the second camiseta. New, lovely materials',
+      120
+    ),
+    new Product(
+      3,
+      'Camiseta 3',
+      'This is the third camiseta. New, lovely materials',
+      130
+    ),
+    new Product(
+      4,
+      'Camiseta 4',
+      'This is the fourth camiseta. New, lovely materials',
+      150
+    ),
+    new Product(
+      5,
+      'Camiseta 5',
+      'This is the fifth camiseta. New, lovely materials',
+      160
+    ),
+    new Product(
+      6,
+      'Camiseta 6',
+      'This is the sixth camiseta. New, lovely materials',
+      180
+    ),
+    new Product(
+      7,
+      'Camiseta 7',
+      'This is the seventh camiseta. New, lovely materials',
+      200
+    ),
+  ];
 
   constructor() {}
+  getProducts(): Product[] {
+    return this.products;
+  }
 }
