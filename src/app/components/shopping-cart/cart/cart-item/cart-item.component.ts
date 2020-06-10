7ec5eeb2c547086d0d7f6f14e4245b76c197813c
Jorge <jorgediazok@gmail.com>
2020-06-10 04:16:59 -0300

    trying to add a delete button

--- src/app/components/shopping-cart/cart/cart-item/cart-item.component.ts
+++ src/app/components/shopping-cart/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,7 +7,12 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CartItemComponent implements OnInit {
   @Input() cartItem: any;
+  @Output() delete: EventEmitter<any> = new EventEmitter();
   constructor() {}
 
   ngOnInit(): void {}
+
+  deleteItem(id: any) {
+    this.delete.emit(id);
+  }
 }
--- src/app/components/shopping-cart/cart/cart.component.ts
+++ src/app/components/shopping-cart/cart/cart.component.ts
@@ -8,12 +8,7 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  cartItems = [
-    // { id: 1, productId: 1, productName: 'Test 1', qty: 4, price: 100 },
-    //{ id: 2, productId: 2, productName: 'Test 2', qty: 2, price: 200 },
-    //{ id: 3, productId: 3, productName: 'Test 3', qty: 5, price: 300 },
-    //{ id: 4, productId: 4, productName: 'Test 4', qty: 1, price: 400 },
-  ];
+  cartItems = [];
 
   cartTotal = 0;
 
@@ -47,5 +42,8 @@ export class CartComponent implements OnInit {
     this.cartItems.forEach((item) => {
       this.cartTotal += item.qty * item.price;
     });
+  }
+  deleteItems(id: any) {
+    this.cartItems = this.cartItems.filter((cartItem) => cartItem.id !== id);
   }
 }
