7949358eb4af86fcda6b09d31d58806f835d6d7e
Jorge <jorgediazok@gmail.com>
2020-07-11 21:39:52 -0300

    signin completed

--- src/backend/src/routes/index.js
+++ src/backend/src/routes/index.js
@@ -14,7 +14,16 @@ router.post("/signup", async (req, res) => {
   const newUser = new User({ email, password });
   await newUser.save();
   const token = jwt.sign({ _id: newUser._id }, "secretsecretword");
-  res.json({ token });
+  res.status(200).json({ token });
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(401).send("The email does not exist");
+  if (user.password !== password) return res.status(401).send("Wrong Password");
+  const token = jwt.sign({ _id: user._id }, "secretsecretword");
+  return res.status(200).json({ token });
 });
 
 module.exports = router;
