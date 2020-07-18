86994e67e835da5a805a7a6906e29d5935c7efcb
Jorge <jorgediazok@gmail.com>
2020-07-18 03:30:41 -0300

    added some comments

--- src/backend/src/routes/index.js
+++ src/backend/src/routes/index.js
@@ -13,8 +13,11 @@ router.get("/", (req, res) => {
 router.post("/signup", async (req, res) => {
   const { email, password } = req.body;
   const newUser = new User({ email, password });
+  //Encriptar password
   newUser.password = await newUser.encryptPassword(password);
+  //Grabar usuario
   await newUser.save();
+  //Generar Token
   const token = jwt.sign({ _id: newUser._id }, "secretsecretword");
   res.status(200).json({ token });
 });
@@ -23,8 +26,10 @@ router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) return res.status(401).send("The email does not exist");
+  //Validar password
   const validPassword = await user.validatePassword(password);
   if (!validPassword) return res.status(401).send("Wrong Password");
+  //Generar token
   const token = jwt.sign({ _id: user._id }, "secretsecretword");
   return res.status(200).json({ token });
 });
